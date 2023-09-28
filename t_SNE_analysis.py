22434d6c9d12efc45a1d0174806d17d3af1ee453
marynavek <mveks001@fiu.edu>
2023-09-28 11:49:15 -0400

    WIP

--- t_SNE_analysis.py
+++ t_SNE_analysis.py
@@ -1,76 +1,41 @@
-import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import StandardScaler
-from sklearn.manifold import TSNE
-import seaborn as sns
 
-real_data = pd.read_csv("test_data/benign_data/sensor_combined.csv")
-generated_data1 = pd.read_csv("test_data/gan_data/comined_data.csv")
+# Sample data: 2 values for 6 items
+items = ['accel X', 'accel Y', 'accel Z', 'gyro X', 'gyro Y', 'gyro Z']
+value1 = [96.40, 97.85, 5.2, 96.8, 98.65, 84.5]
+value2 = [83.75, 92.5, 63.75, 100, 100, 100]
 
-generated_data_ensemble = pd.read_csv("test_data/ensemble_gan_data/egan_all_sensors.csv")
+# Define the width of the bars
+bar_width = 0.4
 
+# Define the x-axis positions for the bars
+x = range(len(items))
 
+# Create the first set of bars
+bar1 = plt.bar(x, value1, width=bar_width, label='Autoencoder', align='center')
 
-spoofed_data = pd.read_csv("test_data/gps_spoofing_data/sensor_combined.csv")
-jammed_data = pd.read_csv("test_data/gps_jamming_data/sensor_combined.csv")
+# Create the second set of bars shifted to the right
+bar2 = plt.bar([i + bar_width for i in x], value2, width=bar_width, label='CNN Classifier', align='center')
 
+# Set labels for the x-axis and y-axis
+plt.xlabel('Sensor', fontsize=16)
+plt.ylabel('Attack Success Rate (ASR)', fontsize=16)
 
-sensor_names = [
-    #"gyro_rad[0]", "gyro_rad[2]",  "xyz[0]"
-    # "accelerometer_m_s2[0]",  "accelerometer_m_s2[1]",  "accelerometer_m_s2[2]",
-    # 
-    # "gyro_x", "gyro_y", "gyro_z", "accel_x", "accel_y", "accel_z", "gps_x", "gps_y", "gps_z", "mag_x", "mag_y", "mag_z"
-    "gyro_rad[0]", "gyro_rad[1]", "gyro_rad[2]",
-    "accelerometer_m_s2[0]", "accelerometer_m_s2[1]", "accelerometer_m_s2[2]"
+# Set the x-axis ticks and labels
+plt.xticks([i + bar_width / 2 for i in x], items,  fontsize=14)
 
-    ] 
+# Add values at the top of each bar
+for bar in bar1 + bar2:
+    height = bar.get_height()
+    plt.annotate(f'{height}', xy=(bar.get_x() + bar.get_width() / 2, height), xytext=(0, 3), textcoords='offset points', ha='center', va='bottom',  fontsize=16)
 
-total_number_of_samples = 1000
-print("Total number of Actual Samples")
-print(total_number_of_samples)
+plt.ylim(0, max(max(value1), max(value2)) + 10)
 
-
-new_real_data = real_data[sensor_names].iloc[:total_number_of_samples]
-new_spoofed_data = spoofed_data[sensor_names].iloc[:total_number_of_samples]
-new_jammed_data = jammed_data[sensor_names].iloc[:total_number_of_samples]
-
-
-new_generated_data = generated_data1[sensor_names].iloc[:total_number_of_samples]
-new_generated_data_ensemble = generated_data_ensemble[sensor_names].iloc[:total_number_of_samples]
-
-new_real_data["label"] = "benign"
-new_jammed_data["label"] = "jammed"
-new_spoofed_data["label"] = "spoofed"
-new_generated_data["label"] = "gan data"
-new_generated_data_ensemble["label"] = "ensemble data"
-
-# data_frames = [new_real_data, new_jammed_data, new_spoofed_data, new_generated_data]
-data_frames = [new_real_data, new_generated_data_ensemble]
-
-all_data = pd.concat(data_frames)
-
-all_data.reset_index(inplace=True)
-# print(all_data)
-sns.pairplot(all_data, hue ='label')
+# Move the legend outside the chart
+# plt.legend(loc='lower center')
+# Add a legend
+# plt.legend(bbox_to_anchor =(0.75, 1.15), ncol = 2)
+plt.legend(bbox_to_anchor =(0.47,-0.35), loc='lower center', ncol=2,  fontsize=16)
+plt.tight_layout()
+# Show the plot
 plt.show()
-# # features = all_data.drop(['label'],1)
-# labels = all_data['label']
-
-# print(labels.head())
-# print(features.head())
-
-# tsne = TSNE(n_components=2, verbose=1, random_state=123)
-# z = tsne.fit_transform(features)
-
-
-# df = pd.DataFrame()
-# df["y"] = labels
-# df["comp-1"] = z[:,0]
-# df["comp-2"] = z[:,1]
-
-# sns.scatterplot(x="comp-1", y="comp-2", hue=df.y.tolist(),
-#                 palette=sns.color_palette("hls", 2),
-#                 data=df).set(title="Iris data T-SNE projection") 
-
-# plt.show()
